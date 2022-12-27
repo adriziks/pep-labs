794b1190285f78af5e8fc1d0e3d623969573193e
adriziks <adriana_rizik@hotmail.com>
2022-12-27 22:30:34 +0000

    Saved progress on Instantiation 2022-12-27 22:30:34 +0000

--- Instantiation/src/main/java/InstantiationExample.java
+++ Instantiation/src/main/java/InstantiationExample.java
@@ -18,6 +18,9 @@ public class InstantiationExample {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+
+        instantiateANewObject a = new instantiateANewObject();
+
+        return a;
     }
 }
