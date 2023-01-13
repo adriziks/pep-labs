448614298dcba371bedc16590e3e851f8d2ddad9
adriziks <adriana_rizik@hotmail.com>
2023-01-13 17:33:00 +0000

    Saved progress on BankAccount 2023-01-13 17:33:00 +0000

--- BankAccount/src/main/java/BankService.java
+++ BankAccount/src/main/java/BankService.java
@@ -23,7 +23,7 @@ public class BankService {
      * @param amount the amount to be deposited.
      */
     public void deposit(double amount){
-
+        this.balance = balance + amount;
     }
 
     /**
@@ -32,6 +32,11 @@ public class BankService {
      * @param amount the amount to be withdrawn.
      */
     public void withdraw(double amount){
+        
+        if (this.balance >= amount) {
+            this.balance = balance - amount;
+        } else {
+        }
 
     }
 
@@ -40,6 +45,6 @@ public class BankService {
      * @return the user's balance.
      */
     public double getBalance(){
-        return 0;
+        return this.balance;
     }
 }
